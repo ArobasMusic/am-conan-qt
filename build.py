38afa8a93d5c1bea04cca5173ecdd1d78155c1a8
NealRame <contact@nealrame.com>
2017-12-12 17:57:26 +0100

    Update build script

--- build.py
+++ build.py
@@ -1,16 +1,13 @@
+from conans.client.loader_parse import load_conanfile_class as ConanFile
 from conan.packager import ConanMultiPackager
 
 def build():
-    builder = ConanMultiPackager(username="amusic", channel="stable")
+    conanfile = ConanFile('./conanfile.py')
+    builder = ConanMultiPackager(
+        reference="{}/{}".format(conanfile.name, conanfile.version),
+        username="amusic",
+    )
     builder.add_common_builds()
-    filtered_builds = []
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings["compiler"] == "Visual Studio":
-            if settings["compiler.runtime"] == "MT" or settings["compiler.runtime"] == "MTd":
-                # Ignore MT runtime
-                continue
-        filtered_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = filtered_builds
     builder.run()
 
 if __name__ == "__main__":
