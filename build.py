dc3c229193875bddf5006b7a4d59b33c871e6fc8
twxs <nicolas.martin@arobas-music.com>
2021-02-02 16:10:04 +0100

    passage à ConanMultiPackager

--- build.py
+++ build.py
@@ -1,90 +1,34 @@
 import os
 import platform
-import re
 
-from conans.client.conan_api import Conan
-from conans.client.loader import ConanFileLoader
+from conanio.packager import ConanMultiPackager
+
+from conans.client.loader import parse_conanfile
 from conans.client.graph.python_requires import ConanPythonRequire
 
-
-def default(col, index, fallback):
-    try:
-        return col[index]
-    except IndexError:
-        return fallback
-
-def get_remote(conan_api):
-    remote = os.getenv('CONAN_UPLOAD', 'http://conan.arobas-music.com@True@arobasmusic').split('@')
-
-    remote_url = remote[0]
-    remote_verify_ssl = default(remote, 1, 'True').lower() in ['true', 'yes']
-    remote_name = default(remote, 2, 'arobasmusic')
-
-    user = os.getenv("CONAN_LOGIN_USERNAME_{}".format(remote_name.upper()), os.getenv("CONAN_LOGIN_USERNAME"))
-    password = os.getenv("CONAN_PASSWORD_{}".format(remote_name.upper()), os.getenv("CONAN_PASSWORD"))
-
-    for registered_remote in conan_api.remote_list():
-        if registered_remote.name == remote_name and registered_remote.url == remote_url:
-            return (remote_name,user,password)
-
-    conan_api.remote_add(remote_name, remote_url, remote_verify_ssl)
-    return (remote_name,user,password)
-
-def get_archs():
-    return map(lambda s: s.strip(), os.getenv('CONAN_ARCHS', 'x86,x86_64').split(','))
-
-def get_compiler():
-    platform_os = platform.system()
-    if platform_os == "Windows":
-        return "Visual Studio"
-    elif platform_os == "Darwin":
-        return "apple-clang"
-    raise Exception("Error unsupported platform: {}".format(platform_os))
-
-def get_compiler_versions():
-    platform_os = platform.system()
-    if platform_os == "Windows":
-        return map(lambda s: s.strip(), os.environ['CONAN_VISUAL_VERSIONS'].split(','))
-    elif platform_os == "Darwin":
-        return map(lambda s: s.strip(), os.environ['CONAN_APPLE_CLANG_VERSIONS'].split(','))
-    raise Exception("Error unsupported platform: {}".format(platform_os))
-
-def get_channel():
-    branch = os.environ['BRANCH_NAME']
-    if re.match(os.getenv("CONAN_STABLE_BRANCH_PATTERN", "^master$"), branch):
-        return "stable"
-    return "testing"
-
-# Create package
-def build(conan_api):
-    compiler = get_compiler()
-    channel = get_channel()
-    for arch in get_archs():
-        for compiler_version in get_compiler_versions():
-            conan_api.create(
-                "./conanfile.py",
-                user="arobasmusic",
-                channel=channel,
-                settings=[
-                    "arch={}".format(arch),
-                    "compiler={}".format(compiler),
-                    "compiler.version={}".format(compiler_version),
-                ]
-            )
-
-# Upload package
-def upload(conan_api):
-    remote, user, password = get_remote(conan_api)
-    conan_api.authenticate(user, password, remote)
-
-    channel = get_channel()
-    conanfile = ConanFileLoader(None, None, ConanPythonRequire(None, None)).load_class('./conanfile.py')
-    conan_api.upload(
-        pattern="{}/{}@arobasmusic/{}".format(conanfile.name, conanfile.version, channel),
-        remote_name=remote
+def build():
+    builder = ConanMultiPackager(
+        build_policy="missing",
+        username="arobasmusic",
+        remotes="http://conan.arobas-music.com"
     )
+    builder.add_common_builds()
+    if platform.system() == "Darwin":
+        os_versions = os.getenv("CONAN_OS_VERSIONS", "").strip()
+
+        if len(os_versions) != 0:
+            builds = []
+            for settings, options, env_vars, build_requires, reference in builder.items:
+                for os_version in map(lambda version: version.strip(), os_versions.split(',')):
+                    builds.append([
+                        {**settings, "os.version": os_version},
+                        options,
+                        env_vars,
+                        build_requires,
+                        reference
+                    ])
+            builder.builds = builds
+    builder.run()
 
 if __name__ == "__main__":
-    (conan_api, _, _) = Conan.factory()
-    build(conan_api)
-    upload(conan_api)
+    build()
