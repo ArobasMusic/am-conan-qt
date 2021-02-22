21e228f1e6543311d3851265f074648bc5fa5044
Julien Graziano <julien@graziano.fr>
2021-02-22 22:56:51 +0100

    build: update build script

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ from conans.client.graph.python_requires import ConanPythonRequire
 def build():
     builder = ConanMultiPackager(
         build_policy="missing",
-        username="arobasmusic",
+        username="arobasmusic"
     )
     builder.add_common_builds()
     if platform.system() == "Darwin":
